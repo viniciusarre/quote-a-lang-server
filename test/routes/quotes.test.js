5d1bd9aa3ddfed565b627d87cfdb0247fd0d78bf
Cain Watson <cainwatson00@gmail.com>
2019-10-08 22:14:24 -0500

    test: POST /getAll returns all quotes

--- test/routes/quotes.test.js
+++ test/routes/quotes.test.js
@@ -15,16 +15,80 @@ describe('/quotes', () => {
   });
 
   beforeEach(async () => {
-    await Quotes.deleteMany({});
+    await Quotes.deleteMany({}).exec();
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await db.disconnect();
   });
 
-  xdescribe('/getAll', () => {
+  describe('/getAll', () => {
     test('POST returns all quotes', async () => {
+      const savedQuotes = await Quotes.create([
+        {
+          _id: 1,
+          autor: 'Lao Tzu',
+          dados: [
+            {
+              fontes: [
+                'https://www.brainyquote.com/quotes/lao_tzu_398196',
+              ],
+              frases: [
+                {
+                  autor: 'Lao Tzu',
+                  language: 'en',
+                  quote: 'Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          _id: 2,
+          autor: 'Pablo Picasso',
+          dados: [
+            {
+              fontes: [
+                'http://spanish-learning-corner.com/pablo-picasso-quotes.html',
+              ],
+              frases: [
+                {
+                  autor: 'Pablo Picasso',
+                  language: 'es',
+                  quote: 'Yo hago lo imposible, porque lo posible lo hace cualquiera.',
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+      const expectedQuotes = [
+        {
+          author: 'Lao Tzu',
+          language: 'fr',
+          quote: {
+            autor: 'Lao Tzu',
+            language: 'en',
+            quote: 'Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.',
+          },
+          source: 'https://www.brainyquote.com/quotes/lao_tzu_398196',
+        },
+        {
+          author: 'Pablo Picasso',
+          language: 'fr',
+          quote: {
+            autor: 'Pablo Picasso',
+            language: 'es',
+            quote: 'Yo hago lo imposible, porque lo posible lo hace cualquiera.',
+          },
+          source: 'http://spanish-learning-corner.com/pablo-picasso-quotes.html',
+        },
+      ];
 
+      await request(app)
+        .post('/quotes/getAll')
+        .send()
+        .expect(200, expectedQuotes);
     });
   });
 
