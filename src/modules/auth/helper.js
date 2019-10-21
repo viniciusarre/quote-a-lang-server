6a0f4bc80cab1fcca7367168cc4c660c86a5cd88
Megan <megan@Megans-MacBook-Pro.local>
2019-10-20 21:10:13 -0700

    adding api documentation and generating using jsdoc

--- src/modules/auth/helper.js
+++ src/modules/auth/helper.js
@@ -1,7 +1,14 @@
 const User = require("../../models/User");
 //make it better
+
+/**
+ * authHelper namespace.
+ * @namespace authHelper
+ */
+
 /**
  * @desc Creates a new user (receives a unique email and a password)
+ * @memberof authHelper
  * @param {string} email
  * @param {string} password
  * @returns {Promise}
@@ -24,6 +31,7 @@ export const login = (email, password) => {
 
 /**
  * @desc Signs up a user which has already been created, it receives email and password and returns a promise containing the operation's result
+ * @memberof authHelper
  * @param {string} user
  * @param {string} email
  * @param {string} password
